ecc1f8a6258467d7631010d855411b42685901e1
Jon Douglas <jodou@microsoft.com>
2021-01-10 14:14:02 -0600

    Initial commit of life CLI on Twitch

---
+++ life/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using life.Commands.Add;
+using life.Commands.View;
+using Spectre.Console.Cli;
+
+namespace life
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            var app = new CommandApp();
+
+            app.Configure(config =>
+            {
+                config.AddExample(new [] {"day", "create"});
+                config.AddExample(new [] {"week", "view", "4"});
+                config.AddBranch<DaySettings>("day", add =>
+                {
+                    add.AddCommand<CreateDayCommand>("create");
+                    add.AddCommand<ViewDayCommand>("view");
+                });
+            });
+
+            return app.Run(args);
+        }
+    }
+}
---
+++ life/Utilities/SettingsDumper.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace life.Utilities
+{
+    public static class SettingsDumper
+    {
+        public static void Dump(CommandSettings settings)
+        {
+            var table = new Table().RoundedBorder();
+            table.AddColumn("[green]Name[/]");
+            table.AddColumn("[red]Value[/]");
+
+            var properties = settings.GetType().GetProperties();
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(settings)
+                    ?.ToString()
+                    ?.Replace("[", "[[");
+
+                table.AddRow(
+                    property.Name,
+                    value ?? "[grey]null[/]");
+            }
+
+            AnsiConsole.Render(table);
+        }
+        
+        public static void DumpStrings(List<string> dump)
+        {
+            var table = new Table().RoundedBorder();
+            table.AddColumn("[green]Name[/]");
+
+            foreach (var d in dump)
+            {
+                table.AddRow(d);
+            }
+
+            AnsiConsole.Render(table);
+        }
+
+        public static void DumpDayView(string day)
+        {
+            var table = new Table().RoundedBorder();
+            table.AddColumn("[green]Name[/]");
+
+            table.AddRow(day);
+
+            AnsiConsole.Render(table);
+        }
+    }
+}
